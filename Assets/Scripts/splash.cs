ac971d3077b419c23e6c95885353aaf34b9caa3d
stephanrb3 <stephanrb3@gmail.com>
2017-11-17 23:39:04 -0500

    Added a splash screen

---
+++ Assets/Scripts/splash.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class splash : MonoBehaviour {
+
+    public AudioSource mainmusic;
+
+	// Use this for initialization
+	void Start () {
+        DontDestroyOnLoad(mainmusic);
+        mainmusic.Play();
+
+        SceneManager.LoadScene(1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
