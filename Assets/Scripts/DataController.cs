4a602166e088e69f6480d27f4f52951b7d894ee7
themost1 <benjaminmost@gmail.com>
2017-11-17 20:23:28 -0500

    Yet again added non-Coldplay material

---
+++ Assets/Scripts/DataController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.IO;
+
+public class DataController : MonoBehaviour {
+
+    //private string gameDataFileName = "data.json";
+    private string gameDataFileName = "Jeff.json";
+
+    void Start()
+    {
+        //DontDestroyOnLoad(gameObject);
+
+        LoadGameData();
+
+        //SceneManager.LoadScene("MainMenu");
+    }
+
+    private void LoadGameData()
+    {
+        // Path.Combine combines strings into a file path
+        // Application.StreamingAssets points to Assets/StreamingAssets in the Editor, and the StreamingAssets folder in a build
+        string filePath = Path.Combine(Application.streamingAssetsPath, gameDataFileName);
+
+        if (File.Exists(filePath))
+        {
+            // Read the json from the file into a string
+            string dataAsJson = File.ReadAllText(filePath);
+            // Pass the json to JsonUtility, and tell it to create a GameData object from it
+            GameData loadedData = JsonUtility.FromJson<GameData>(dataAsJson);
+        }
+        else
+        {
+            Debug.LogError("Cannot load game data!");
+        }
+    }
+}
+
